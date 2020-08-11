afa27c07c6d2635cb3651366414d77c3ad43bb1c
XanderUZZZER <xanderuzzzer@gmail.com>
2020-08-11 17:21:35 +0300

    vid 219

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['string'] }));
 app.use(router);
 
 app.listen(3000, () => {
-  console.log('Port 3000 listen');
+  console.log('http://localhost:3000');
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,53 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('he there, you');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email && password && email === 'email' && password === 'pass') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('invalid email or pass');
+  };
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <h1>You are logged in</h1>
+      <a href="/logout">Logout</a>
+    </div>
+  `);
+  } else {
+    res.send(`
+    <div>
+      <h1>You are not logged in</h1>
+      <a href="/login">Login</a>
+    </div>
+  `);
+  }
 });
 
 export { router };
